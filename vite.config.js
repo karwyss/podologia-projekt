d90e38fa11cb3157db791533985f7e9776e2c645
nxx9005 <karolwysoglad00@gmail.com>
2026-01-20 22:16:56 +0100

    fix error5

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: './src',
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: './',  // ważne dla deploymentu
+  base: './',
   build: {
-    outDir: '../dist',   // wyjdzie z src do root/dist
+    outDir: 'dist',
     sourcemap: false,
     rollupOptions: {
-      // input: 'index.jsx',  // względnie do root czyli ./src/index.jsx
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
